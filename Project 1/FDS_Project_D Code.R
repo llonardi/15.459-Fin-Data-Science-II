bebc2d94a635d368e58da50aa84e45d4f02d9e20
shounak16 <43579890+shounak16@users.noreply.github.com>
2018-11-11 16:44:10 -0500

    Update FDS_Project_D Code.R

--- Project 1/FDS_Project_D Code.R
+++ Project 1/FDS_Project_D Code.R
@@ -19,10 +19,10 @@ library(tau)
 library(wordcloud)
 
 # Read in data
-Parent_train <- read.csv("/Users/hermannviktor/Dropbox/MIT/Courses/2. Fall Term/15.458 Data Science/Assignments/Assignment 4/H1_Data.csv",sep=";")
-Child_train <- read.csv("/Users/hermannviktor/Dropbox/MIT/Courses/2. Fall Term/15.458 Data Science/Assignments/Assignment 4/Heirarchies_Data.csv")
-testerBitch <- read.csv("/Users/hermannviktor/Dropbox/MIT/Courses/2. Fall Term/15.458 Data Science/Assignments/Assignment 4/Data_proj_D_test.csv")
-
+Parent_train <- read.csv("H1_Data.csv")
+Child_train <- read.csv("Heirarchies_Data.csv")
+testerBitch <- read.csv("Proj_D_test.csv")
+rm(newsDF)
 # Slicing testerBitch into H1 and lower classes
 
 testerBitch$cat <- trimws(testerBitch$cat) 
@@ -40,12 +40,12 @@ parent_total <- rbind(Parent_train, parent_test)
 parent_total$cat <- trimws(parent_total$cat) 
 
 #Document Term Matrix
-doc_matrix <- create_matrix(parent_total$article[1:4000], language="english", removeNumbers=TRUE,
+doc_matrix <- create_matrix(parent_total$article, language="english", removeNumbers=TRUE,
                             stemWords=TRUE, removeSparseTerms=.998)
 
 #Container
-container <- create_container(doc_matrix,as.numeric(factor(parent_total$cat)), trainSize=1:2000,
-                              testSize=2001:4000, virgin=FALSE)  
+container <- create_container(doc_matrix,as.numeric(factor(parent_total$cat)), trainSize=1:7000,
+                              testSize=(length(Parent_train$cat)+1):(length(Parent_train$cat)+7001), virgin=FALSE)  
 
 #Training Models
 SVM <- train_model(container,"SVM")
@@ -54,32 +54,79 @@ SVM_CLASSIFY <- classify_model(container, SVM) #3 MANGOOOOO
 SLDA <- train_model(container,"SLDA")
 SLDA_CLASSIFY <- classify_model(container, SLDA) #2 Laura
 
-BAGGING <- train_model(container,"BAGGING")
-BAGGING_CLASSIFY <- classify_model(container, BAGGING)#very sexual tbh
+#BAGGING <- train_model(container,"BAGGING")
+#BAGGING_CLASSIFY <- classify_model(container, BAGGING)#very sexual tbh
 
-NNET <- train_model(container,"NNET")
-NNET_CLASSIFY <- classify_model(container, NNET) #1
+#NNET <- train_model(container,"NNET")
+#NNET_CLASSIFY <- classify_model(container, NNET) #1
 
 GLMNET = train_model(container,"GLMNET")
 GLMNET_CLASSIFY = classify_model(container,GLMNET)
 
+LGLMNET=train_model(container,"GLMNET",maxitglm = 10^6)
+LGLMNET_CLASSIFY = classify_model(container,LGLMNET)
+
 MAXENT = train_model(container, "MAXENT")
 MAXENT_CLASSIFY = classify_model(container,MAXENT)
 
-BOOSTING = train_model(container, "BOOSTING")
-BOOSTING_CLASSIFY = classify_model(container, BOOSTING)
+Y1_MAXENT = train_model(container, "MAXENT", l1_regularizer = 1, use_sgd = TRUE)
+Y1_MAXENT_CLASSIFY = classify_model(container,Y1_MAXENT)
 
+Y2_MAXENT = train_model(container, "MAXENT", l2_regularizer = 1)
+Y2_MAXENT_CLASSIFY = classify_model(container,Y2_MAXENT)
+
+Y3_MAXENT = train_model(container, "MAXENT", set_heldout = 0.2*7000)
+Y3_MAXENT_CLASSIFY = classify_model(container,Y3_MAXENT)
+
+
+#Total Analytics
 analytics_total = create_analytics(container, 
-                                   cbind(SVM_CLASSIFY,SLDA_CLASSIFY,BAGGING_CLASSIFY, NNET_CLASSIFY,GLMNET_CLASSIFY, MAXENT_CLASSIFY, BOOSTING_CLASSIFY))
+                                     cbind(SVM_CLASSIFY, SLDA_CLASSIFY, MAXENT_CLASSIFY,GLMNET_CLASSIFY))
 summary(analytics_total)
 
+#Ensemble Analytics
+
+#MAXENT and GLMNET
+MGanalytics_total = create_analytics(container, 
+                                   cbind(MAXENT_CLASSIFY,GLMNET_CLASSIFY))
+summary(MGanalytics_total)
+
+#MAXENT and SVM
+MSanalytics_total = create_analytics(container, 
+                                   cbind(MAXENT_CLASSIFY,SVM_CLASSIFY))
+summary(MSanalytics_total)
+
+#SVM and GLMNET
+SGanalytics_total = create_analytics(container, 
+                                   cbind(SVM_CLASSIFY,GLMNET_CLASSIFY))
+summary(SGanalytics_total)
+
+#Fine-Tune Analytics
+Y1analytics_total = create_analytics(container, 
+                                   Y1_MAXENT_CLASSIFY)
+summary(Y1analytics_total)
+
+Y2analytics_total = create_analytics(container, 
+                                     Y2_MAXENT_CLASSIFY)
+summary(Y2analytics_total)
+
+Y3analytics_total = create_analytics(container, 
+                                     Y3_MAXENT_CLASSIFY)
+summary(Y3analytics_total)
+
+Lanalytics_total = create_analytics(container, 
+                                     LGLMNET_CLASSIFY)
+summary(Lanalytics_total)
+
+EnsembleAnal_total=create_analytics(container, 
+                                    cbind(Y3_MAXENT_CLASSIFY,LGLMNET_CLASSIFY))
+summary(EnsembleAnal_total)
 
 # 6.Cross-Validation
 SVM <- cross_validate(container, 4, "SVM")
 SLDA <- cross_validate(container, 4, "SLDA")
-BAGGING <- cross_validate(container, 4, "BAGGING")
-NNET <- cross_validate(container, 4, "NNET")
-
+MAXENT <- cross_validate(container, 4, "MAXENT")
+GLMNET <- cross_validate(container, 4, "GLMNET")
 
 # 7.Exporting Data
 write.csv(analytics@document_summary, "DocumentSummary.csv")
