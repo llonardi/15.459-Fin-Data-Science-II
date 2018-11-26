98b9f809549cadaea44ffc5547605bf784a0ee25
llonardi <43709556+llonardi@users.noreply.github.com>
2018-11-26 13:16:30 -0500

    Update Q3 Code.R

--- Project 2/Q3 Code.R
+++ Project 2/Q3 Code.R
@@ -1,11 +1,15 @@
 library(readxl)
+library(ggplot2)
+
 setwd("C:/Users/Laura/Desktop/MIT Fall 2018/Financial Data Science/Assignment/Assignment #5")
 
-DF= read_excel("ProjectE Q3 Final Clean.xlsx")
+DF = read_excel("ProjectE Q3 Final Clean.xlsx")
 
 DF_90 = subset(DF, DF$id_strike==90)
 DF_95 = subset(DF, DF$id_strike==95)
 DF_100 = subset(DF, DF$id_strike==100)
+
+ 
 
 sf <- function (DF, action, Q)
 # DF = data frame of 1 strike
@@ -17,7 +21,7 @@ sf <- function (DF, action, Q)
   colnames(m) <- c("d", "Q", "q", "Cash","MV", "MV_temp", "PnL")
   m$d = DF$d
 
-  for (i in 1:ncol(DF))
+  for (i in 1:nrow(DF))
   {
     # First Trading Day Set Up
     if (i == 1)
@@ -78,9 +82,29 @@ sf <- function (DF, action, Q)
   return(m)  
 }
 
-test = sf(DF_90, "Short", Q = 35)
-head(test)
+call_90 = sf(DF_90, "Short", Q = 35)
+call_95 = sf(DF_95, "Short", Q = 35)
+call_100 = sf(DF_100, "Short", Q = 35)
 
+write.csv(DF_90, "test.csv")
+write.csv(call_90, "test2.csv")
+
+### a
+
+plot_MV <- ggplot(call_90, aes(x = as.Date(call_90$d))) + geom_line(aes(y = call_90$MV, color = "90 Strike - MV")) + 
+  geom_line(aes(y = call_95$MV, color = "95 Strike - MV")) +
+  geom_line(aes(y = call_100$MV, color = "100 Strike - MV")) + 
+  scale_colour_manual("Legend",values = c("90 Strike - MV"="blue","95 Strike - MV"="red", "100 Strike - MV"="green")) +
+  labs(title = "Short Call Market Value", x = "Date", y = "Market Value")
+print(plot_MV)
+
+ ### g
+plot_delta <- ggplot(DF_90, aes(x = as.Date(DF_90$d))) + geom_line(aes(y = DF_90$delta, color = "90 Strike - Delta")) + 
+  geom_line(aes(y = DF_95$delta, color = "95 Strike - Delta")) +
+  geom_line(aes(y = DF_100$delta, color = "100 Strike - Delta")) + 
+  scale_colour_manual("Legend",values = c("90 Strike - Delta"="blue","95 Strike - Delta"="red", "100 Strike - Delta"="green")) +
+  labs(title = "Short Call Delta", x = "Date", y = "Delta")
+print(plot_delta)
 
 
 
